d7847e2a54ce7a36f7e7ea1c7a8e8728fb0feb9a
Lotuk1991 <lotuk1991@gmail.com>
2025-05-01 23:53:38 +0200

    Update handlers_lot.py

--- handlers_lot.py
+++ handlers_lot.py
@@ -2,7 +2,7 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from bot import dp
-from copart_parser import get_lot_info
+from copart_lot_parser import get_lot_info
 from iaai_parser import get_iaai_lot_info
 
 # Состояния
