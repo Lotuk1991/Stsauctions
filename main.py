df22a8458f062401354ff23f1699c7696d58107a
Lotuk1991 <lotuk1991@gmail.com>
2025-05-01 08:52:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from handlers import register_handlers
 from config import BOT_TOKEN
+from handlers import lot_handler
 
+# Налаштування логування
 logging.basicConfig(level=logging.INFO)
 
+# Ініціалізація бота і диспетчера
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-register_handlers(dp)
+# Реєстрація хендлера
+dp.register_message_handler(lot_handler, commands=["lot"])
 
-@dp.message_handler(commands=['start'])
+# Обробник команди /start
+@dp.message_handler(commands=["start"])
 async def start_handler(message: types.Message):
-    await message.answer("Привіт! Введи команду /copart bmw 3 щоб знайти авто на Copart.")
+    await message.answer("Привіт! Введи команду /lot 47813034 щоб знайти авто на Copart.")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
