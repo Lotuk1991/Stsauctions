a6f3829f444bf67c20ee39f107e450d4cfac4b5e
Lotuk1991 <lotuk1991@gmail.com>
2025-05-01 23:52:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # main.py
 from aiogram import executor
 from bot import dp
-import handlers
 import handlers_lot  # <-- обязательно
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
