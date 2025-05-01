680cf5de33173d78135585029944e450c14edb12
Lotuk1991 <lotuk1991@gmail.com>
2025-05-01 09:47:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from config import BOT_TOKEN
-from handlers import register_handlers
+from handlers import lot_handler  # вот этот импорт обязателен!
 
-
-# Налаштування логування
+# Логирование
 logging.basicConfig(level=logging.INFO)
 
-# Ініціалізація бота і диспетчера
+# Бот и диспетчер
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
-# Реєстрація хендлера
+# Регистрация хендлера /lot
 dp.register_message_handler(lot_handler, commands=["lot"])
 
-# Обробник команди /start
+# Обработчик /start
 @dp.message_handler(commands=["start"])
 async def start_handler(message: types.Message):
     await message.answer("Привіт! Введи команду /lot 47813034 щоб знайти авто на Copart.")
