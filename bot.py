debce616098570c1552ef95220c8a8e879a05caf
Lotuk1991 <lotuk1991@gmail.com>
2025-05-01 15:14:23 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,7 @@
+# bot.py
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
