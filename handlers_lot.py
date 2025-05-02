a18ea1b5556c6dc1d8a39736fb785297ecc85828
Lotuk1991 <lotuk1991@gmail.com>
2025-05-03 01:10:38 +0200

    Update handlers_lot.py

--- handlers_lot.py
+++ handlers_lot.py
@@ -40,6 +40,7 @@ async def parse_lot(message: types.Message, state: FSMContext):
     if auction == "copart":
         result = get_lot_info(lot_id)
     else:
-        result = await get_iaai_full_info(lot_id)
+        result = get_iaai_full_info(lot_id)
+
 
     await message.answer(result, parse_mode="HTML")
